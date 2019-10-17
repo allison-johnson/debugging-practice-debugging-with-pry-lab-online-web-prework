5ae1aca3ed44f249e1584531ef34155271c39cc1
Allison Johnson <allison.cuttler@gmail.com>
2019-10-17 01:04:21 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,7 +12,9 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| crew_member = "Hello #{crew_member}."}
+  crew.each do |crew_member| 
+    crew_member = "Hello #{crew_member}."
+  end #each
 end
 
 def engage
